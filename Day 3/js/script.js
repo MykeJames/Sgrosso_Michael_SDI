935ba9031c593dc3d620e66eafdbc928a3fca63f
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-10 20:18:00 -0500

    This is long over due oops
    Setup basic files, started coding

---
+++ Day 3/js/script.js
@@ -0,0 +1,104 @@
+/*
+Michael Sgrosso FSS# 63757
+1/101/2015
+Day 3 Coding
+ */
+
+// Single lined comment
+
+// This is an alert
+// alert("Testing to see if this is connected");
+
+//Console.log - allows programmers to see information
+console.log("This is the console");
+
+
+// Declare a variable
+// Use the keyword of var
+
+var whatever1;    // Declaration of a variable
+whatever1 = 42;   // Definition of a variable
+
+console.log(whatever1);
+
+// Declare and define a variable
+
+var a = 2;
+console.log(a);
+
+// Simple math
+
+a = a+3;
+console.log(a);
+
+var b;
+b = a+3;
+console.log(b);
+console.log(a);
+
+// Find our age
+var birthYear = 1973;
+
+// Age is calculated by current year minus birth year
+var currentYear = 2015;
+
+var age = currentYear - birthYear;
+console.log(age - 1);
+
+// Math
+// +,-.*,/
+
+// Find area of a triangle
+// 1/2 * base * height
+var base = 6.5;
+var height = 10.2;
+
+var areaTriangle = .5 * base * height;
+console.log(areaTriangle);
+
+// Modulo - %
+// Gives the remainder
+var remainder = 32%10;
+console.log(remainder)
+
+// Find if even or odd
+// Modulo by 2 %2
+// 1 if it is odd
+// 0 if even
+var evenOrOdd = 67%2;
+console.log(evenOrOdd)
+
+// Assignment Operator
+/*
+= Assignment
+++ Adds 1 to the variable
+-- Subtracts 1 from the variable
++= a # Addition Assignment
+-= a # Subtraction Assignment
+/= a # Division Assignment
+*= a # Multiplication Assignment
+ */
+
+var counter = 1;
+counter++; // counter = counter +1
+console.log(counter);
+
+counter--; // counter = counter - 1
+console.log(counter);
+
+counter +=3; // counter = counter +3
+console.log(counter);
+
+counter -=2; // counter = counter - 2
+console.log(counter);
+
+counter /=2; // counter = counter /2
+console.log(counter);
+
+counter *=4; // counter = counter *4
+console.log(counter);
+
+
+
+
+
