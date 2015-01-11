183e097919c465c7026531fd278f158005c69127
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-10 22:29:25 -0500

    Slice of pie part 2
    define variables

--- Day3LabExpressions/js/script.js
+++ Day3LabExpressions/js/script.js
@@ -16,6 +16,22 @@ var pizzasOrdered; // number of pizzas ordered
 var slicesPerPerson; // number of slices for each person
 
 
+// Slice of pie part 2
+
+// Declare and define variables
+var slicesPerPie = 8; // number of slices per pizza
+var numberOfPeople = 10; // number of people
+var pizzasOrdered = 4; // number of pizzas ordered
+var slicesPerPerson = (slicesPerPie * pizzasOrdered) / numberOfPeople; // number of slices per person
+
+
+var sparkySlices = (slicesPerPie * pizzasOrdered)%numberOfPeople; //number of slices sparky gets to eat
+
+// console.log(slicesPerPerson);
+
+// Display number of slices sparky gets to eat
+console.log("Sparky got" +" "+ sparkySlices +" "+ "slices of pizza.");
+
 
 
 
