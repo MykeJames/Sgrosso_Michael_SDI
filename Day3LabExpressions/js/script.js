7179a7f2dbfe2142f24aec6c667e6c67de8efa15
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-10 22:50:54 -0500

    Average shopping bill
    Define and declare variables

--- Day3LabExpressions/js/script.js
+++ Day3LabExpressions/js/script.js
@@ -8,29 +8,45 @@
 // Slice of pie part 1
 // How many slices of pizza will each person at the party get, with the given data?
 
-// Declare variables
-
-var slicesPerPie; // number of slices per pizza
-var numberOfPeople; // number of people
-var pizzasOrdered; // number of pizzas ordered
-var slicesPerPerson; // number of slices for each person
-
-
-// Slice of pie part 2
-
 // Declare and define variables
+
 var slicesPerPie = 8; // number of slices per pizza
 var numberOfPeople = 10; // number of people
 var pizzasOrdered = 4; // number of pizzas ordered
 var slicesPerPerson = (slicesPerPie * pizzasOrdered) / numberOfPeople; // number of slices per person
 
+// Display number of slices each person gets to eat
+console.log("Each person ate " + slicesPerPerson +" "+ "slices of pizza.");
 
-var sparkySlices = (slicesPerPie * pizzasOrdered)%numberOfPeople; //number of slices sparky gets to eat
-
-// console.log(slicesPerPerson);
+// Slice of pie part 2
+//number of slices sparky gets to eat
+var sparkySlices = (slicesPerPie * pizzasOrdered)%numberOfPeople;
 
 // Display number of slices sparky gets to eat
 console.log("Sparky got" +" "+ sparkySlices +" "+ "slices of pizza.");
+
+
+// Average shopping bill
+// Calculate the average cost of shopping for the last five weeks
+
+// Declare and define variables
+
+var week1;
+var week2;
+var week3;
+var week4;
+var week5;
+
+var totalSpent;
+var avgSpentWeek;
+
+// Discounts
+
+var origPrice;
+var discountPercent;
+var itemDescription
+var taxPercent;
+
 
 
 
