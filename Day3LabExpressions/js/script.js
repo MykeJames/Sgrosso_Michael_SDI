04058f82090211026bb9c1798263fc5e7a94a761
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-10 23:58:24 -0500

    Average Shopping Bill Discounts
    Declare and define variables

--- Day3LabExpressions/js/script.js
+++ Day3LabExpressions/js/script.js
@@ -31,21 +31,38 @@ console.log("Sparky got" +" "+ sparkySlices +" "+ "slices of pizza.");
 
 // Declare and define variables
 
-var week1;
-var week2;
-var week3;
-var week4;
-var week5;
+var week1 = 154.65;
+var week2 = 74.75;
+var week3 = 83.25;
+var week4 = 123.55;
+var week5 = 65.95;
 
-var totalSpent;
-var avgSpentWeek;
+var totalSpent = week1 + week2 + week3 + week4 + week5; // Find total spent for 5 weeks
+var avgSpentWeek = totalSpent/5; // Find average spent per week
+
+// Round decimal to 3 places
+avgSpentWeek = avgSpentWeek.toFixed(3);
+
+
+// Display total amount spent and average spent per week
+console.log("You have spent a total	of $" + totalSpent + " on groceries over 5 weeks. That is an average of $" + avgSpentWeek + " per week");
+
+
 
 // Discounts
+// Declare and define variables
 
-var origPrice;
-var discountPercent;
-var itemDescription
-var taxPercent;
+var origPrice = 30.00; // Original price
+var discountPercent = .1; // discount percentage
+var itemDescription = "Top round roast";
+var taxPercent = .06; // tax percentage
+
+var priceNoTax = origPrice - (origPrice * discountPercent); // Find discounted price pre tax
+var priceWithTax = priceNoTax + (priceNoTax * taxPercent); // Find discounted price post tax
+
+// Display original price, discount percentage, discounted price without tax and discounted price with tax
+console.log("Your " + itemDescription + "was originally $" + origPrice + ", but after a " + (discountPercent * 100)  + "% discount, it is now $" + priceNoTax + " without tax, and $" + priceWithTax + " with tax");
+
 
 
 
