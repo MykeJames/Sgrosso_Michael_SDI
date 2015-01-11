737905b0a089fba6dfc036e346a93daa3cd8d696
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-10 22:13:47 -0500

    Slice of pie part 1
    create html file and js file
    declare variables

---
+++ Day3LabExpressions/js/script.js
@@ -0,0 +1,25 @@
+/*
+ Michael Sgrosso FSS# 63757
+ 1/101/2015
+ SDI
+ Day 3 Lab Expressions
+ */
+
+// Slice of pie part 1
+// How many slices of pizza will each person at the party get, with the given data?
+
+// Declare variables
+
+var slicesPerPie; // number of slices per pizza
+var numberOfPeople; // number of people
+var pizzasOrdered; // number of pizzas ordered
+var slicesPerPerson; // number of slices for each person
+
+
+
+
+
+
+
+
+
