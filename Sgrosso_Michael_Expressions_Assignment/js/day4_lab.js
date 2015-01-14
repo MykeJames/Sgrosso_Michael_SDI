c8120728fc24aff327dc5cbf1bafcd55e1591520
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-13 23:39:53 -0500

    Array and arithmatic
    added array and mathematical equations

--- Sgrosso_Michael_Expressions_Assignment/js/day4_lab.js
+++ Sgrosso_Michael_Expressions_Assignment/js/day4_lab.js
@@ -8,12 +8,24 @@ Day 4 Lab Expression Assignment
 
 //alert("Testing for html connection");
 
+// Blood alcohol content calculator
 
 // Define and declare variables
 // Prompt user for required information - ( Number of drinks, time drinking, users weight in pounds)
 var drinkQty = prompt("How many drinks have you consumed?");
 var hours = prompt("How many hours have you been drinking?");
 var weight = prompt("Please enter your weight in pounds");
+
+// Define array for variable values
+var userData = [drinkQty, weight, hours]
+
+// Define a variable and equation using array items and at least 2 assignment operators
+var bac = (userData[0] * 0.06 * 100 * 1.055);
+
+bac /= userData[1]; // division equals
+
+bac -= (userData[2] * 0.015); // minus equals
+
 
 
 
