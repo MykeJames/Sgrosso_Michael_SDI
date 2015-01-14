0cad1f92bfaabf0e4a3fa78e27cd4a4e54af90d9
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-13 23:36:46 -0500

    Define variables and prompts
    3 variable prompts

--- Sgrosso_Michael_Expressions_Assignment/js/day4_lab.js
+++ Sgrosso_Michael_Expressions_Assignment/js/day4_lab.js
@@ -9,3 +9,13 @@ Day 4 Lab Expression Assignment
 //alert("Testing for html connection");
 
 
+// Define and declare variables
+// Prompt user for required information - ( Number of drinks, time drinking, users weight in pounds)
+var drinkQty = prompt("How many drinks have you consumed?");
+var hours = prompt("How many hours have you been drinking?");
+var weight = prompt("Please enter your weight in pounds");
+
+
+
+
+
