a8b9172039e66c74f661d92ee41f0a4623b9d2b4
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-13 19:57:39 -0500

    Learned user prompting

--- Day 4/js/day4_prompting.js
+++ Day 4/js/day4_prompting.js
@@ -8,3 +8,26 @@
 
 //alert("Testing for connection to html file");
 
+// Ask user for input - prompt();
+
+// prompt("Enter your year of birth");
+
+// Save user response - by creating a variable
+var birthYear = prompt("Enter your year of birth");
+console.log(birthYear);
+
+
+// Calculate area of a rectangle
+// area = length * width
+
+// Create 2 variable and prompt the user length and width
+
+var length = prompt("Let's calculate the area of a rectangle. \n please enter the length of the rectangle");
+
+var width = prompt("Please enter the width of the rectangle");
+
+// Calculate area
+var areaRect = length * width;
+console.log("The area of you rectangle is" + areaRect);
+console.log("The area of your rectangle with a length of " +length+  " and a width of " +width+ " is " +areaRect);
+
