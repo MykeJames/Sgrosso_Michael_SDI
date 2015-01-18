2a51f3225af4f774bb1781a43316e0f5b6b96f4f
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-18 00:13:43 -0500

    Prompts and variables
    update flowchart
    forst prompt and variable

--- Sgrosso_Michael_Conditionals_Assignment/js/lab6.js
+++ Sgrosso_Michael_Conditionals_Assignment/js/lab6.js
@@ -1,4 +1,4 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 /*
 Michael Sgrosso
@@ -6,4 +6,33 @@ SDI Campus 1501
 1/17/2015
 Day 6 Lab Conditionals Assignment
  */
+
+// Auto loan calculator
+
+// define vaiables for loan amount, interest rate and loan term, then prompt for user input
+
+
+var num1;
+var num2;
+var payment;
+
+var loanAmt     = prompt("Please enter the loan amount in US dollars");
+    if (loanAmt === "" || loanAmt === NaN) {
+    loanAmt = prompt("Please enter a valid number");
+    if ((loanAmt === "" || loanAmt === NaN)) {
+        console.log("Your entry is invalid, reload page and start again")
+    }
+} else {
+    
+
+
+
+
+
+
+
+
+
+
+
 
