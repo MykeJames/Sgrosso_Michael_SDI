cac833ee993bb1ef97eaabbd9049957b854f15ef
Mychael Sgrosso <msgrosso@fullsail.edu>
2015-01-18 00:23:09 -0500

    Comments and Nesting
    add comments and format nesting

--- Sgrosso_Michael_Conditionals_Assignment/js/lab6.js
+++ Sgrosso_Michael_Conditionals_Assignment/js/lab6.js
@@ -9,21 +9,23 @@ Day 6 Lab Conditionals Assignment
 
 // Auto loan calculator
 
-// define vaiables for loan amount, interest rate and loan term, then prompt for user input
-
-
+// define vaiablesfor calculations
 var num1;
 var num2;
 var payment;
 
-var loanAmt     = prompt("Please enter the loan amount in US dollars");
-    if (loanAmt === "" || loanAmt === NaN) {
-    loanAmt = prompt("Please enter a valid number");
-    if ((loanAmt === "" || loanAmt === NaN)) {
-        console.log("Your entry is invalid, reload page and start again")
+// define vaiables for loan amount, interest rate and loan term, and prompt for user input
+var loanAmt     = prompt("Please enter the loan amount in US dollars"); // prompt for user input
+    if (loanAmt === "" || loanAmt === NaN) { //validation for number and entry
+        loanAmt = prompt("Please enter a valid number"); // reprompt for invalid entry
+        if (loanAmt === "" || loanAmt === NaN) { //revalidate
+            console.log("Your entry is invalid, reload page and start again") // second invalid entry console.log
     }
 } else {
     
+
+
+
 
 
 
